9c6e01f627d0f6cc4c6e142e53f9c3def2d0b052
hafsaxfarid <hafsaxfarid@gmail.com>
2022-02-17 21:30:04 -0500

    Lab 4 Files Created
    Made lab 4 from lab 3

---
+++ Lab4_HF/Assets/[Scripts]/CameraController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Player Camera")]
+    public float mouseSensitivity = 10.0f;
+    public Transform playerBody;
+
+    private float XRotation = 0.0f;
+    
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        XRotation -= mouseY;
+        XRotation = Mathf.Clamp(XRotation, -90.0f, 90.0f);
+
+        transform.localRotation = Quaternion.Euler(XRotation, 0.0f, 0.0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
