816213fbe30b4df3392320f9e31e3689945cd0e1
hafsaxfarid <hafsaxfarid@gmail.com>
2022-03-13 01:00:01 -0500

    Saving + Loading 2
    Tested in LD Player saving and loading using binary formatting

--- Lab5_HF/Assets/[Scripts]/GameSaveManager.cs
+++ Lab5_HF/Assets/[Scripts]/GameSaveManager.cs
@@ -2,8 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// SAVEING + LOADING using Binary Formatting
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+[System.Serializable]
+class SaveData
+{
+    public float playerPositionX;
+    public float playerPositionY;
+    public float playerPositionZ;
+}
+
 public class GameSaveManager : MonoBehaviour
 {
+    // SAVEING + LOADING using Binary Formatting
+    public Transform player;
+    void SaveGame()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/MySaveData.dat");
+        SaveData data = new SaveData();
+        data.playerPositionX = player.position.x;
+        data.playerPositionY = player.position.y;
+        data.playerPositionZ = player.position.z;
+        bf.Serialize(file, data);
+        file.Close();
+        Debug.Log("Game data saved!");
+    }
+    void LoadGame()
+    {
+        if (File.Exists(Application.persistentDataPath + "/MySaveData.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/MySaveData.dat", FileMode.Open);
+            SaveData data = (SaveData)bf.Deserialize(file);
+            file.Close();
+            var x = data.playerPositionX;
+            var y = data.playerPositionY;
+            var z = data.playerPositionZ;
+
+
+            player.gameObject.GetComponent<CharacterController>().enabled = false;
+            player.position = new Vector3(x, y, z);
+            player.gameObject.GetComponent<CharacterController>().enabled = true;
+
+            Debug.Log("Game data loaded!");
+        }
+        else
+        {
+            Debug.LogError("There is no save data!");
+        }
+    }
+
+    void ResetData()
+    {
+        if (File.Exists(Application.persistentDataPath + "/MySaveData.dat"))
+        {
+            File.Delete(Application.persistentDataPath + "/MySaveData.dat");
+            Debug.Log("Data reset complete!");
+        }
+        else
+        {
+            Debug.LogError("No save data to delete.");
+        }
+    }
+
+    // SAVEING + LOADING using PlayerPrefs
+    /*
     public Transform player;
 
     // serialize player data
@@ -26,9 +93,7 @@ public class GameSaveManager : MonoBehaviour
             var z = PlayerPrefs.GetFloat("PlayerPositionZ");
 
             player.gameObject.GetComponent<CharacterController>().enabled = false;
-
             player.position = new Vector3(x, y, z);
-
             player.gameObject.GetComponent<CharacterController>().enabled = true;
 
             Debug.Log("Game data loaded!");
@@ -44,6 +109,7 @@ public class GameSaveManager : MonoBehaviour
         PlayerPrefs.DeleteAll();
         Debug.Log("Data reset complete");
     }
+*/
 
     public void SaveButtonPressed()
     {
@@ -54,4 +120,6 @@ public class GameSaveManager : MonoBehaviour
     {
         LoadGame();
     }
+
+    
 }
