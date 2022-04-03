12f3bf7675bd12137d5ae792845187fa44c38a5c
hafsaxfarid <hafsaxfarid@gmail.com>
2022-04-02 22:58:53 -0400

    Created more tiles to test
    voxel, big tile and massive tiles

--- Lab8_HF/Assets/[Scripts]/MapGenerator.cs
+++ Lab8_HF/Assets/[Scripts]/MapGenerator.cs
@@ -9,20 +9,20 @@ public class MapGenerator : MonoBehaviour
     public Transform spawnPoint;
 
     [Header("World Properties")]
-    [Range(8, 64)]
+    [Range(8, 128)]
     public int width = 8;
 
-    [Range(8, 64)]
+    [Range(8, 128)]
     public int height = 8;
     
-    [Range(8, 64)]
+    [Range(8, 128)]
     public int depth = 8;
 
     [Header("Scaling Values")]
-    [Range(8, 64)]
+    [Range(8, 128)]
     
     public float min = 16f;
-    [Range(8, 64)]
+    [Range(8, 128)]
     public float max = 24f;
 
     [Header("Tile Properties")]
@@ -96,7 +96,10 @@ public class MapGenerator : MonoBehaviour
                     if(y < perlinValue)
                     {
                         // instantiate 3D Tile
-                        var tile = Instantiate(threeDTile, new Vector3(x, y, z), Quaternion.identity);
+                        var tile = Instantiate(threeDTile, new Vector3(x * threeDTile.transform.localScale.x,
+                            y * threeDTile.transform.localScale.y,
+                            z * threeDTile.transform.localScale.z), Quaternion.identity);
+                        
                         tile.transform.SetParent(tileParent);
                         grid.Add(tile);
                     }
@@ -198,7 +201,10 @@ public class MapGenerator : MonoBehaviour
     private void PositionPlayer()
     {
         player.gameObject.GetComponent<CharacterController>().enabled = false;
-        player.position = new Vector3(width * 0.5f, height + 5f, depth * 0.5f);
+        player.position = new Vector3(width * 0.5f * threeDTile.transform.localScale.x,
+            height * threeDTile.transform.localScale.y + 5f,
+            depth * 0.5f * threeDTile.transform.localScale.z);
+        
         spawnPoint.position = player.position;
         player.gameObject.GetComponent<CharacterController>().enabled = true;
     }
